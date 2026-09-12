a038e838a7bb9c60b04afe797d27ee10b27d04dc
lscate <info@lscate.ru>
2026-09-12 04:34:04 +0300

    ышеу

--- script.js
+++ script.js
@@ -1961,6 +1961,34 @@
     if(row) row.classList.remove("is-error");
   }
 
+  /* Yandex Metrica 111456931. Load only after analytics consent. */
+  function loadYandexMetrika(){
+    if(window.__eventmaksMetrikaLoaded) return;
+    window.__eventmaksMetrikaLoaded=true;
+
+    (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for(let j=0;j<e.scripts.length;j+=1){
+        if(e.scripts[j].src===r) return;
+      }
+      k=e.createElement(t);
+      a=e.getElementsByTagName(t)[0];
+      k.async=1;
+      k.src=r;
+      a.parentNode.insertBefore(k,a);
+    })(window,document,"script","https://mc.yandex.ru/metrika/tag.js?id=111456931","ym");
+
+    window.ym(111456931,"init",{
+      ssr:true,
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true
+    });
+  }
+
+  if(readEntryConsent()==="accepted") loadYandexMetrika();
+
   if(entryConsent){
     const stored=readEntryConsent();
 
@@ -1978,6 +2006,7 @@
 
       writeEntryConsent(accepted ? "accepted" : "declined");
       syncQuizConsentFromEntry(accepted);
+      if(accepted) loadYandexMetrika();
       closeEntryConsent();
 
       window.setTimeout(()=>{
